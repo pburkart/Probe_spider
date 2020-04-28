15509ded974bc56efc9c2aa9222c84cfd589fe8e
Paul Burkart <pburkart99@gmail.com>
2020-04-28 04:48:06 -0400

    Update cmsdetect.py

--- webosint/cmsdetect.py
+++ webosint/cmsdetect.py
@@ -7,11 +7,14 @@ W = '\033[0m'  # white
 
 def CMSdetect(domain, port):
     print ( W + '[+]' + G + 'Checking Targer ...'+W+'\n')
+    
     payload = {'key': '1641c3b9f2b1c8676ceaba95d00f7cf2e3531830c5fa9a6cc5e2d922b2ed7165dcce66', 'url': domain}
     cms_url = "https://whatcms.org/APIEndpoint/Detect"
+    
     response = requests.get(cms_url, params=payload)
     cms_data = response.json()
     cms_info = cms_data['result']
+    
     if cms_info['code'] == 200:
         print('Detected CMS     : %s' % cms_info['name'])
         print('Detected Version : %s' % cms_info['version'])
