7d0c5b54b4db7df2cddf32735377538a1d39666b
Paul Burkart <pburkart99@gmail.com>
2020-04-28 05:08:09 -0400

    Update maildb.py
    Fixed grammar and directories

--- maildb.py
+++ maildb.py
@@ -9,13 +9,13 @@ def maildb():
     emailaddress=input(C+"Enter the Email Address (Eg:test@gmail.com): "+W)
     if  ("@" and ".com") or ("@" and ".in") in emailaddress:
         print ( W + '[+]' + G + "Data Breaching....." + '\n'+W)
-        os.system("h8mail -t "+emailaddress+" -o /root/Downloads/osint/src/output.csv > /root/Downloads/osint/src/log")
-        f=open("/root/Downloads/osint/src/output.csv","r")
+        os.system("h8mail -t "+emailaddress+" -o src/output.csv > src/log")
+        f=open("src/output.csv","r")
         line=f.readlines()
         if len(line) > 1:
             for i in line:
             	print(i)
         else:
-            print(G+"Data breached is Not Compromised")
+            print(G+"E-Mail Address not found in known data breaches")
     else:
         print(R+"Error: Invalid Email Address")
