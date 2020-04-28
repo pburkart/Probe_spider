39b7b7fb4c011513c56aa24a6f8f005b39ce87f9
Paul Burkart <pburkart99@gmail.com>
2020-04-28 04:57:49 -0400

    Update cmsdetect.py
    Fixed typo

--- webosint/cmsdetect.py
+++ webosint/cmsdetect.py
@@ -6,7 +6,7 @@ C = '\033[36m' # cyan
 W = '\033[0m'  # white
 
 def CMSdetect(domain, port):
-    print ( W + '[+]' + G + 'Checking Targer ...'+W+'\n')
+    print ( W + '[+]' + G + 'Checking Target ...'+W+'\n')
     
     payload = {'key': '1641c3b9f2b1c8676ceaba95d00f7cf2e3531830c5fa9a6cc5e2d922b2ed7165dcce66', 'url': domain}
     cms_url = "https://whatcms.org/APIEndpoint/Detect"
